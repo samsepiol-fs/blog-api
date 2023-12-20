3fc55501f86d8c874a2346b5ec513f8655201710
samsepiol-fs <shukla.pritesh214@gmail.com>
2023-12-20 23:45:20 +0530

    add read update and delete blog api routes

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,4 +1,5 @@
 import BlogPost from '../models/blog.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const createBlog = async (req, res, next) => {
     const { title, content, author } = req.body;
@@ -7,6 +8,60 @@ export const createBlog = async (req, res, next) => {
     try {
         await blogPost.save();
         res.status(200).json('Blog created successfully!');
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const readBlog = async (req, res, next) => {
+    try {
+        const blogPosts = await BlogPost.find();
+        if(blogPosts.length === 0) {
+            return next(errorHandler(404, "No blogs found!"));
+        }
+        res.status(200).json(blogPosts);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getBlog = async (req, res, next) => {
+    try {
+        const blog = await BlogPost.findById(req.params.id);
+        if(!blog) {
+            return next(errorHandler(404, "Blog not foun!"));
+        }
+        res.status(200).json(blog);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteBlog = async (req, res, next) => {
+    const blog = await BlogPost.findById(req.params.id);
+    if(!blog) {
+        return next(errorHandler(404, "Blog not found!"));
+    }
+    try {
+        await BlogPost.findByIdAndDelete(req.params.id);
+        res.status(200).json("Blog has been deleted successfully!");
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateBlog = async (req, res, next) => {
+    const blog = await BlogPost.findById(req.params.id);
+    if(!blog) {
+        return next(errorHandler(404, "Blog not found!"));
+    }
+    try {
+        const updatedBlog = await BlogPost.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        );
+        res.status(200).json(updatedBlog);
     } catch (error) {
         next(error);
     }
--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import {createBlog} from '../controllers/blog.controller.js';
+import {createBlog, readBlog, getBlog, deleteBlog, updateBlog} from '../controllers/blog.controller.js';
 
 const router = express.Router();
 
 router.post(`/create`, createBlog);
-
+router.get(`/read`, readBlog);
+router.get(`/read/:id`, getBlog);
+router.delete('/delete/:id', deleteBlog);
+router.post(`/update/:id`, updateBlog);
 export default router;
